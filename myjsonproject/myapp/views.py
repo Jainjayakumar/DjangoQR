cc2c0c07892f9033eba5f7b9da0c6129b42a293e
Jainjayakumar <36849292+Jainjayakumar@users.noreply.github.com>
2019-08-01 11:59:01 +0530

    Update views.py

--- myjsonproject/myapp/views.py
+++ myjsonproject/myapp/views.py
@@ -11,7 +11,7 @@ from base64 import b64encode, b64decode
 
 
 def homePageView(request):
-    return HttpResponse('Hello, World! Jain is Home')
+    return HttpResponse('Hello, World! This is Home Page')
 
 @csrf_exempt
 def methodinfo(request):
